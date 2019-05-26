bb8e08adbaef9d5fbb4acd20c368d10adfb2788e
Mateusz Biłgorajski <bilgorajskim@gmail.com>
2019-05-26 09:48:42 +0200

    Fixes

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const spawn = require("child_process").spawn;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const YT_API_KEY = process.env.YT_API_KEY;
 const STREAMER = process.env.STREAMER;
-const DEBUG = process.env.DEBUG;
+const DEBUG = false;
+const CHECK_INTERVAL = 60;
+
+let streamerProcess = null;
+let lastStart = 0;
+let lastLive = 0;
+let isLive = false;
+
+let uptime = 0;
+let downtime = 0;
+let runtime = 0;
+
+let lastLoopTime = null;
 
 async function checkIfChannelIsLive(channelId) {
   try {
     const resp = await fetch(`
-        https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&type=video&eventType=live&key=${YT_API_KEY}
+        https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&type=video&eventType=live&key=${YT_API_KEY}
         `);
     const json = await resp.json();
     if (DEBUG) {
@@ -32,11 +44,6 @@ async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let streamerProcess = null;
-let lastStart = 0;
-let lastLive = 0;
-let isLive = false;
-
 async function startStreamer() {
   if (streamerProcess) {
     try {
@@ -48,12 +55,25 @@ async function startStreamer() {
   try {
     if (STREAMER.includes("bash")) {
       const scriptPath = STREAMER.split("bash ")[1];
-      streamerProcess = spawn("bash", [scriptPath]);
+      streamerProcess = spawn("/bin/bash", [scriptPath]);
     } else if (STREAMER.includes(".sh")) {
-      streamerProcess = spawn("bash", [STREAMER]);
+      streamerProcess = spawn("/bin/bash", [STREAMER]);
     } else {
       streamerProcess = spawn(STREAMER);
     }
+    if (DEBUG) {
+      streamerProcess.stdout.on("data", data => {
+        console.log(`stdout: ${data}`);
+      });
+
+      streamerProcess.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+      });
+    }
+    streamerProcess.on("close", code => {
+      console.log(`Streamer process exited with code ${code}`);
+      streamerProcess = null;
+    });
   } catch (err) {
     console.error("Failed to start streamer:", err);
     return;
@@ -62,23 +82,33 @@ async function startStreamer() {
   console.info("Streamer started.");
 }
 
+function printUptime() {
+  if (uptime <= 0 || runtime <= 0) {
+    return;
+  }
+  const uptimePercent = Math.round((uptime / runtime) * 10000) / 100;
+  console.log(`Uptime: ${uptimePercent}%`);
+}
+
 async function loop() {
   while (true) {
-    let wasLive = isLive;
-    isLive = await checkIfChannelIsLive(CHANNEL_ID);
     if (!streamerProcess) {
       await startStreamer();
     } else {
+      let wasLive = isLive;
+      isLive = await checkIfChannelIsLive(CHANNEL_ID);
       if (isLive) {
         if (!wasLive) {
           console.info("YT stream is online.");
+          printUptime();
         }
         lastLive = Date.now();
       } else {
         if (wasLive) {
           console.warn(
-            "YT stream went offline. Will restart streamer in 5 minutes."
+            "YT stream is offline. Will restart streamer if it won't go back online in 5 minutes."
           );
+          printUptime();
         }
       }
 
@@ -92,7 +122,18 @@ async function loop() {
         await startStreamer();
       }
     }
-    await sleep(1000 * 30);
+    let timeProgression = 0;
+    if (lastLoopTime) {
+      timeProgression = Date.now() - lastLoopTime;
+    }
+    if (isLive) {
+      uptime += timeProgression;
+    } else {
+      downtime += timeProgression;
+    }
+    runtime += timeProgression;
+    lastLoopTime = Date.now();
+    await sleep(1000 * CHECK_INTERVAL);
   }
 }
 
